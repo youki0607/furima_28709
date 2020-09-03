e94a211394cdcfbc147050ab41484c52662899da
youki0607 <its_showtime@i.softbank.jp>
2020-09-03 15:50:04 +0900

    テストコードの実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    before do
+        @user = FactoryBot.build(:user)
+      end
+      
+    context '新規登録がうまくいくとき' do
+      it "nameとemail、passwordとpassword_confirmation,family_name,first_name,family_name_kana,firsy_name_kanaが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "nicknameが存在すれば登録できる" do
+        @user.nickname = "キン肉マン"
+        expect(@user).to be_valid
+      end
+      it "passwordは6文字以上であれば登録できる" do
+        @user.password = "aiueo1"
+        @user.password_confirmation = "aiueo1"
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空では登録できない" do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "emailがからでは登録できない" do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "emailが一意性でなければ登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "passwordが空では登録できないで" do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it "passwordが5文字以下であれば登録できない" do
+        @user.password = "12345"
+        @user.password_confirmation = "12345"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it "passwordは半角英数字混合でなければ登録できない" do
+        @user.password = "111111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it "名字がなければ登録できないこと" do
+        @user.family_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+      it "名前がなければ登録できないこと" do
+        @user.first_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it "ユーザーの苗字は全角(漢字・ひらがな・カタカナ)で入力させなければ登録できないこと" do
+        @user.family_name = "nakazawa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name 全角文字を使用してください")
+      end
+      it "ユーザーの名前は全角(漢字・ひらがな・カタカナ)で入力しなければ登録できないこと" do
+        @user.first_name = "yuuki"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name 全角文字を使用してください")
+      end
+      it "ユーザーの苗字のフリガナがなければ登録できないこと" do
+        @user.family_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+      end
+      it "ユーザーの名前のフリガナがなければ登録できないこと" do
+        @user.first_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it "ユーザーの苗字のフリガナは全角(カタカナ)で入力しなければ登録できないこと" do
+        @user.family_name_kana = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana カタカナを使用してください")
+      end
+      it "ユーザーの名前のフリガナは全角(カタカナ)で入力しなければ登録できないこと" do
+        @user.first_name_kana = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana カタカナを使用してください")
+      end
+      it "生年月日が必須であること" do
+        @user.birthday = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
+end
