91c31500564f3883c308c306d5400b4bf10b0995
youki0607 <its_showtime@i.softbank.jp>
2020-09-03 16:08:53 +0900

    テストコードの実装2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday])
@@ -13,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"キン肉マン"}
-    email                 {"test@com"}
+    nickname              { 'キン肉マン' }
+    email                 { 'test@com' }
     password = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    family_name           {"中沢"}
-    first_name            {"優貴"}
-    family_name_kana      {"ナカザワ"}
-    first_name_kana       {"ユウキ"}
-    birthday              {Faker::Date.birthday}
+    password              { password }
+    password_confirmation { password }
+    family_name           { '中沢' }
+    first_name            { '優貴' }
+    family_name_kana      { 'ナカザワ' }
+    first_name_kana       { 'ユウキ' }
+    birthday              { Faker::Date.birthday }
   end
 end
