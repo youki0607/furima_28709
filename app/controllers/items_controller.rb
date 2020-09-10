9c15f9ad99a419d8b377a0fb4c02b436821f6380
youki0607 <its_showtime@i.softbank.jp>
2020-09-10 18:56:24 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destory, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destory]
 
   def index
     @item = Item.all.order("created_at DESC")
