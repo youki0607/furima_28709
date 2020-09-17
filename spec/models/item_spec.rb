9fde13170eaa0ae5cd9debba899468bcfa514041
youki0607 <its_showtime@i.softbank.jp>
2020-09-18 01:10:17 +0900

    rubocop編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -81,9 +81,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
       it 'priceの範囲が￥9.999.999より下であること' do
-        @item.price = 10_000_000
+        @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than 9999999')
+        expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
     end
   end
