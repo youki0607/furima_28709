3a961f482d5fdbdeb75ef90207d2f6af2b603ec4
youki0607 <its_showtime@i.softbank.jp>
2020-09-17 20:52:33 +0900

    keyの環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,43 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
 
   def index
-    
+    @item = Item.find(params[:item_id])
+    @order= UserOrder.new
+    if @item.user_id == current_user.id || @item.order!= nil
+      return redirect_to root_path
+    end
+  end
+
+  def new
+    @order = Order.new
   end
 
   def create
-    
+    @order= UserOrder.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.permit(:item_id, :token, :name, :price, :postal_code, :prefectures, :city, :house_number, :build_number, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
