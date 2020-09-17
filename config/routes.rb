ebac560b15064c0933e27ed3ebf8a7f1a959a385
youki0607 <its_showtime@i.softbank.jp>
2020-09-17 20:53:52 +0900

    条件分岐追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :orders, only:[:create]
+  resources :items do
+    resources :orders, only:[:create, :index]
+  end
 end
